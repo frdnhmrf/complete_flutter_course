bb395232690f3a00236bfb852f7c14a6b75f91e2
frdnhmrf <frdnhmrf@gmail.com>
2022-12-19 22:54:27 +0700

    Done Until Section Riverpod

---
+++ ecommerce_app_feature_first/lib/src/common_widgets/async_value_widget.dart
@@ -0,0 +1,80 @@
+import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shimmer/shimmer.dart';
+
+class AsyncValueWidget<T> extends StatelessWidget {
+  const AsyncValueWidget({
+    super.key,
+    required this.value,
+    required this.data,
+  });
+
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
+      loading: () => SizedBox(
+        width: 200.0,
+        height: 100.0,
+        child: Shimmer.fromColors(
+          baseColor: Colors.grey[300]!,
+          highlightColor: Colors.grey[700]!,
+          child: ListView.builder(
+            itemBuilder: (_, __) => Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 48.0,
+                    height: 48.0,
+                    color: Colors.white,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: double.infinity,
+                          height: 8.0,
+                          color: Colors.white,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 2.0),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: 8.0,
+                          color: Colors.white,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 2.0),
+                        ),
+                        Container(
+                          width: 40.0,
+                          height: 8.0,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            itemCount: 6,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ ecommerce_app_feature_first/lib/src/features/products/data/fake_products_repository.dart
@@ -0,0 +1,51 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeProductsRepository {
+  final _products = kTestProducts;
+
+  List<Product> getProductList() {
+    return _products;
+  }
+
+  Product? getProduct(String id) {
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchPrdouctsList() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchPrdouctsList() async* {
+    await Future.delayed(const Duration(seconds: 2));
+    yield _products;
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchPrdouctsList()
+        .map((products) => products.firstWhere((product) => product.id == id));
+  }
+}
+
+final productRepoitoryProvider = Provider<FakeProductsRepository>((ref) {
+  return FakeProductsRepository();
+});
+
+final productsListStreamProvider =
+    StreamProvider.autoDispose<List<Product>>((ref) {
+  final productsRepository = ref.watch(productRepoitoryProvider);
+  return productsRepository.watchPrdouctsList();
+});
+
+final productsListFutureProvider = FutureProvider<List<Product>>((ref) {
+  final productsRepository = ref.watch(productRepoitoryProvider);
+  return productsRepository.fetchPrdouctsList();
+});
+
+final productProvider =
+    StreamProvider.autoDispose.family<Product?, String>((ref, id) {
+  final productRepository = ref.watch(productRepoitoryProvider);
+  return productRepository.watchProduct(id);
+});
